bd610cfb3e04f71a6123120fc8b24ecda1ae7b45
Andrew Harvey <andrew.harvey4@gmail.com>
2017-04-08 16:12:55 +1000

    fix build

--- main.js
+++ main.js
@@ -1,4 +1,2 @@
-module.exports = exports = {
-    Threebox: require('./src/Threebox'),
-    THREE: require('./src/three64.js')
-}
+window.Threebox = require('./src/Threebox');
+window.THREE = require('./src/three64.js');
